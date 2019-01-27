69fec09e208071bb67fd7dd7bac87ea1aa602f16
chauffemain <46550908+chauffemain@users.noreply.github.com>
2019-01-27 12:16:58 -0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactContactCard
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+  <div className="App">
+    <h1>Hello CodeSandbox</h1>
+    <h2>Start editing to see some magic happen!</h2>
+  </div>
+);
+
+export default App;
